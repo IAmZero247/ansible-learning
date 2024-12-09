d8c290ce05bb0727f3cab7246a507c5a6cb17530
ruby <smilelikegirls@gmail.com>
2024-12-09 17:38:58 +0530

    updated

--- lab-setup-using-ubuntu/ansible/copy-ssh-keys.sh
+++ lab-setup-using-ubuntu/ansible/copy-ssh-keys.sh
@@ -1,3 +1,10 @@
+#ssh-keygen command
+#==================
+#-b Specifies the key size
+#-t key_type [Specifies the algorithm used for key generation. Possible values are "rsa", "dsa", and "ecdsa". The default is "rsa".]
+#-f specifies the filename of the key file.
+#-N passphase [Here empty]
+
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ""
 
 for user in ruby root
